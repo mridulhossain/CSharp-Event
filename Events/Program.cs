ded26dd6cd730fed54143119f0005925ff2fafad
mridulhossain <mridulfeni94@gmail.com>
2021-09-30 03:03:47 +0600

    events

---
+++ Events/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Events
+{
+    class Program
+    {
+        // create delegate for signiture
+        public delegate void Noti(string msg);
+
+        // create event type of delegates
+        public static event Noti MsgSender;
+
+        static void Main(string[] args)
+        {
+            //create connetion
+            MsgSender += SmsSend;
+            MsgSender += EmailSend;
+
+
+            // invoke with data
+            MsgSender.Invoke("Hello,thank you for subscribe.");
+        }
+
+
+
+
+        //method
+        public static void SmsSend(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void EmailSend(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+    }
+}
